06ffee7c6262da8cfa7de6431d8850de8e236b19
MartianDominic <leadsist1@gmail.com>
2026-04-18 00:51:42 +0300

    chore: merge executor worktree (08-06) + resolve stores/index conflict

--- apps/web/src/stores/index.ts
+++ apps/web/src/stores/index.ts
@@ -1,12 +1,9 @@
 export { useClientStore } from "./clientStore";
 export type { ClientStore } from "./clientStore";
-<<<<<<< HEAD
 export { useAnalyticsStore } from "./analyticsStore";
 export { useContentCalendarStore } from "./contentCalendarStore";
 export { useIntelligenceStore } from "./intelligenceStore";
-=======
 export { useArticleEditorStore, DEFAULT_ARTICLE } from "./articleEditorStore";
 export type { ArticleEditorState, ArticleStatus, GenerationStatus } from "./articleEditorStore";
 export { useArticleLibraryStore } from "./articleLibraryStore";
 export type { Article, SortField, SortDir } from "./articleLibraryStore";
->>>>>>> worktree-agent-a44ba7ad
